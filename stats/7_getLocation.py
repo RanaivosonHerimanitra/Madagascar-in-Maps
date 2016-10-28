27fe6431a7d4abf3b75a5a0d24e66c5339521c7d
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2016-10-28 09:34:23 +0300

    implement location search

--- stats/7_getLocation.py
+++ stats/7_getLocation.py
@@ -15,45 +15,38 @@ import numpy as np
 import sys  
 ##
 potential_crimes= pd.read_csv("data/potential_crimes_stories.csv",sep=";",encoding="utf-8")
+code_ocha= pd.read_csv("data/code_OCHA.csv",sep=",",encoding="utf-8")
 ##
-years = range(2003,2017)
-months= ["01","02","03","04","05","06","07","08","09","10","11","12"]
-days= range(1,31)
-date_combinations = product(years,months,days)
-conversion_mois={"January":'Janvier',"February":"Fevrier","March":"mars","April":"avril",
-                "May":"mai","June":"juin","July":"juillet","August":"aout","September":"septembre",
-                "October":"octobre","November":"novembre","December":"decembre"}
+potential_crimes= potential_crimes.dropna()
+potential_crimes.index = range(potential_crimes.shape[0])
 ##
 #initialization:
-def search_date_in (form="year/month",var='link'):
-    if 'date' not in potential_crimes.columns:
-        potential_crimes['date']=''
-    for x in date_combinations:
-        y,m,d = x
-        if form=="year/month":
-            mymonth = str(y) + "/" + m
-        if form=="year-month":
-            mymonth = str(y) + "-" + m
-        if form=="text":
-            mymonth= date(y, int(m), d)
-            mymonth=mymonth.strftime("%d %B %Y")
-            d0= d.split()[1]
-            mymonth=mymonth.replace(d0,conversion_mois[d0])
+def search_location_in (invar='story',newvar='location',fromvar='FOKONTANY'):
+    if newvar not in potential_crimes.columns:
+        potential_crimes[newvar]=''
+    for x in code_ocha[fromvar]:
         #retrieve index
-        i = [i for i,j in enumerate(potential_crimes[var]) if mymonth in j and potential_crimes.loc[i,var]!=[] ]
+        i = [i for i,j in enumerate(potential_crimes[invar]) if x in j and potential_crimes.loc[i,newvar]!=[] ]
         if len(i)==1:
-            print mymonth,i ,  potential_crimes.loc[i, var]
-            potential_crimes.loc[i, 'date']=mymonth
+            print (x,i ,  potential_crimes.loc[i, invar])
+            potential_crimes.loc[i, newvar]=x
         else:
             for i1 in i:
-                print mymonth, i1 ,  potential_crimes.loc[i1,var]
-                potential_crimes.loc[i1, 'date']=mymonth
-                
-##run all functions that search for dates
-search_date_in(form="year/month",var='link')
-search_date_in(form="year-month",var='link')
-search_date_in(form="year/month",var='story')
-search_date_in(form="year-month",var='story')
-search_date_in(form="text",var='link')
-search_date_in(form="text",var='story')
+                print (x, i1 ,  potential_crimes.loc[i1,invar])
+                potential_crimes.loc[i1,newvar]=x
+###
+search_location_in(invar='story',newvar='location_fkt_story',fromvar='FOKONTANY')
+search_location_in(invar='story',newvar='location_fkt_story',fromvar='FOKONTANY')
+search_location_in(invar='story',newvar='location_reg_story',fromvar='REGION')
+search_location_in(invar='story',newvar='location_reg_story',fromvar='REGION')
+search_location_in(invar='story',newvar='location_com_story',fromvar='COMMUNE')
+search_location_in(invar='story',newvar='location_com_story',fromvar='COMMUNE')
+###
+search_location_in(invar='link',newvar='location_fkt_link',fromvar='FOKONTANY')
+search_location_in(invar='link',newvar='location_fkt_link',fromvar='FOKONTANY')
+search_location_in(invar='link',newvar='location_reg_link',fromvar='REGION')
+search_location_in(invar='link',newvar='location_reg_link',fromvar='REGION')
+search_location_in(invar='link',newvar='location_com_link',fromvar='COMMUNE')
+search_location_in(invar='link',newvar='location_com_link',fromvar='COMMUNE')
+### save
 potential_crimes.to_csv("data/potential_crimes_stories.csv",sep=";",encoding="utf-8")
